c3cb8efb164ebdffa0541b5a9846a801e6f730a7
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-07-23 14:27:35 -0700

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        const sCount: Record<string,number> = {};
+        const tCount: Record<string, number> = {};
+
+        for (let i = 0; i < s.length; i++) {
+            sCount[s[i]] = (sCount[s[i]] || 0) + 1;
+            tCount[t[i]] = (tCount[t[i]] || 0) + 1;
+        }
+
+        for (const key in sCount) {
+            if (sCount[key] !== tCount[key]) {
+                return false;
+            }
+        }
+        return true
+    }
+}

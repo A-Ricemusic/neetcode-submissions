363dc261cb2f26663d84f65a5428bd5d5e70d614
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-07-24 14:39:11 -0700

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        let res: number = 0;
+        let charSet = new Set<string>(s);
+
+        for (let c of charSet) {
+            let count: number = 0;
+            let l: number = 0;
+            for (let r = 0; r < s.length; r++) {
+                if (s[r] === c) {
+                    count++;
+                }
+                while (r - l + 1 - count > k) {
+                    if (s[l] === c) {
+                        count--;
+                    }
+                    l++;
+                }
+                res = Math.max(res, r - l + 1)
+            }
+        }
+
+        return res;
+
+    }
+}

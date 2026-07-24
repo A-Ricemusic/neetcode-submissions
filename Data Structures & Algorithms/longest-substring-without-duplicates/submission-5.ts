5d2a927e3738d47c314266e4b1521763b51bac73
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-07-24 10:31:40 -0700

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        let charSet = new Set<string>();
+        let res: number = 0;
+        let l: number = 0;
+
+        for (let r = 0; r < s.length; r++) {
+            while (charSet.has(s[r])) {
+                charSet.delete(s[l])
+                l++;
+            }
+
+            charSet.add(s[r]);
+            res = Math.max(res, r - l + 1)
+
+        }
+
+        return res;
+    }
+}

76928d12081c91d4dc9af820bba226a3a34fb8b6
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-07-27 12:01:12 -0700

    Add: combination-target-sum-ii - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-4.ts
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[]} candidates
+     * @param {number} target
+     * @return {number[][]}
+     */
+    combinationSum2(candidates: number[], target: number): number[][] {
+
+        let res: number[][] = [];
+        candidates.sort((a, b) => a - b);
+
+        const dfs = (idx, curr: number[], total: number): void => {
+
+
+            if (idx === candidates.length && total === target) {
+
+                res.push([...curr]);
+                return;
+            }
+
+            if (total > target || idx === candidates.length) {
+                return
+            }
+
+            let next: number = idx
+
+            while (next < candidates.length && candidates[idx] === candidates[next] ) {
+                next++;
+            } 
+            dfs(next, curr, total)
+            curr.push(candidates[idx])
+            dfs(idx + 1, curr, total + candidates[idx])
+            curr.pop()
+        }
+
+        dfs(0,[], 0)
+
+        return res
+    }
+}

6eedb0741122946e3735f53bb21e2727d8889303
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-07-28 13:43:46 -0700

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} people
+     * @param {number} limit
+     * @return {number}
+     * 
+     * [1,2,4,5] L = 6
+     * 
+     */
+    numRescueBoats(people: number[], limit: number): number {
+        let l: number = 0
+        let r: number = people.length - 1;
+        let res: number = 0;
+        people.sort((a,b) => a - b);
+        while (l < r) {
+            const total = people[l] + people[r]
+            if (total <= limit) {
+                l++;
+                r--;
+            } else {
+                r--;
+            }
+            res++;
+        }
+
+        if (l === r) {
+            res++;
+        }
+
+        return res
+    }
+}

69681575ffa1ab97f0d1bc4919e4fa237cea3b5d
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-07-27 17:35:31 -0700

    Add: permutations-ii - submission-1

---
+++ Data Structures & Algorithms/permutations-ii/submission-1.ts
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     * 
+     * [1,1,2,4,6]
+     */
+    permuteUnique(nums: number[]): number[][] {
+        let mySet = new Set<string>();
+        let res: number[][] = [];
+        let cache: boolean[] = new Array(nums.length).fill(false)
+
+        const dfs = (curr: number[]) => {
+            if (curr.length === nums.length) {
+                let key: string = curr.join('')
+                if (!mySet.has(key)) {
+                    res.push([...curr])
+                    mySet.add(key)
+                }
+                return
+            }
+
+            for (let j = 0; j < nums.length; j++) {
+                if (!cache[j]) {
+                    cache[j] = true;
+                    curr.push(nums[j]);
+                    dfs(curr)
+                    cache[j] = false;
+                    curr.pop()
+                }
+            }
+
+        }
+
+        dfs([])
+        return res
+    }
+}

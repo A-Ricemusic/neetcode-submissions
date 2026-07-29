799780744d6d32490b479a8074a0364fe9cf3d30
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-07-29 09:44:25 -0700

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums: number[], target: number): boolean {
+        //o(n) not optimal:
+
+        for (let num of nums) {
+            if (num === target) {
+                return true
+            }
+        }
+
+        return false
+    }
+}

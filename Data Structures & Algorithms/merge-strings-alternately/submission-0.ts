e4b141ba0748eccdd9e90c71d569b1c5fb41f48c
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-07-29 17:29:03 -0700

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} word1
+     * @param {string} word2
+     * @return {string}
+     */
+    mergeAlternately(word1: string, word2: string): string {
+        let res: string = "";
+
+        for (let i = 0; i < word1.length; i++) {
+            res += word1[i]
+            if (i < word2.length) {
+                res += word2[i]
+            }
+        }
+        if (word2.length > word1.length) {
+            for (let i = word1.length; i < word2.length; i++) {
+                res += word2[i]
+            }
+        }
+
+        return res
+
+    }
+}

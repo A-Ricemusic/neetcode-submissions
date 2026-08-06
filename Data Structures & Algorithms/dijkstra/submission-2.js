f6f8b2bdd3cf6e98832f958bff7a3e6dcbfbe3f8
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-05 18:51:58 -0700

    Add: dijkstra - submission-2

---
+++ Data Structures & Algorithms/dijkstra/submission-2.js
@@ -0,0 +1,39 @@
+/**
+ * const PriorityQueue = require('priority-queue-js');
+ */
+
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @param {number} src
+     * @returns {Object}
+     */
+    shortestPath(n, edges, src) {
+        const res = {};
+        const adj = Array.from({length: n }, () => [])
+        for (const [x,y,z] of edges) {
+            adj[x].push([y,z])
+        }
+
+        for (let i = 0; i < n; i++) {
+            res[i] = -1;
+        }
+
+        const heap = new MinPriorityQueue((x) => x[0]);
+        heap.enqueue([0,src])
+        while (heap.size() > 0) {
+            const [cost, node] = heap.dequeue()
+            if (res[node] !== -1) {
+                continue
+            }
+            res[node] = cost
+            for (const [x,y] of adj[node]) {
+                if (res[x] === -1) {
+                    heap.enqueue([cost + y, x])
+                }
+            }
+        }
+        return res;
+    }
+}

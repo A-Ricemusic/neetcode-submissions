51a0241d5e46c0687711d275454dbc99fa1f8355
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-08 09:41:17 -0700

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n: number): number {
+        if (n <= 2) {
+            return n;
+        }
+
+        const dp: number[] = new Array(n).fill(-1);
+        dp[0] = 1;
+        dp[1] = 2;
+
+        const dfs = (i: number): number => {
+            const num1 = dp[i - 1] !== -1 ? dp[i - 1] : dfs(i - 1);
+            const num2 = dp[i - 2] !== -1 ? dp[i - 2] : dfs(i - 2);
+            dp[i] = num1 + num2;
+            return dp[i];
+        };
+        return dfs(n - 1);
+    }
+}

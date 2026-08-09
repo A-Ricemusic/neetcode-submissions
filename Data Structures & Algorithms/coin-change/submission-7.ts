bd89ece548f4a21571819bc2fd23639a283aebf2
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-09 11:58:17 -0700

    Add: coin-change - submission-7

---
+++ Data Structures & Algorithms/coin-change/submission-7.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins: number[], amount: number): number {
+        if (amount === 0) return 0
+        const rows: number = coins.length;
+        const cols: number = amount + 1;
+        const dp: number[][] = Array.from({length: rows}, () => new Array(cols).fill(-1));
+
+        const dfs = (i: number, curr: number): number => {
+            if (curr === amount) return 0;
+            if (i >= coins.length || curr > amount) return Infinity;
+            if (dp[i][curr] !== -1) return dp[i][curr]
+
+            const res = Math.min(dfs(i, curr + coins[i]) + 1, dfs(i + 1, curr),
+            dfs(i + 1, curr + coins[i]) + 1)
+            dp[i][curr] = res;
+            return res
+            
+        }
+
+
+        let res: number = dfs(0,0)
+        return res === Infinity? -1 : res
+    }
+}

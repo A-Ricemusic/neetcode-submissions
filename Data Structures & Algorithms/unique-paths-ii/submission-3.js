ffcd11a86ee7f48b6d7df71485584e448e07baed
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-11 14:13:01 -0700

    Add: unique-paths-ii - submission-3

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    uniquePathsWithObstacles(grid) {
+        if (!grid ||!grid[0].length) return 0;
+        const rows = grid.length;
+        const cols = grid[0].length;
+        const dp = new Array(cols).fill(0);
+        dp[0] = grid[0][0] === 1? 0 : 1;
+
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                if (grid[r][c] === 1) {
+                    dp[c] = 0;
+                } else if (c > 0) {
+                    dp[c] += dp[c - 1];
+                }
+            }
+        }
+
+        return dp[cols - 1]
+
+    }
+}

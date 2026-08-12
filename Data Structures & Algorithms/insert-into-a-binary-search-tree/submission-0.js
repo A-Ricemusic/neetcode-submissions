56bbdf8bfb30e70443b10f34663af106f1ed4e19
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-12 10:26:52 -0700

    Add: insert-into-a-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/insert-into-a-binary-search-tree/submission-0.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} val
+     * @return {TreeNode}
+     */
+    insertIntoBST(root, val) {
+        const newNode = new TreeNode(val)
+        if (!root) {
+            root = newNode;
+            return root;
+        }
+        let curr = root
+        while (curr) {
+            if (val > curr.val) {
+                if (curr.right === null) {
+                    curr.right = newNode
+                    return root;
+                }
+                curr = curr.right
+            } else if (val < curr.val) {
+                if (curr.left === null) {
+                    curr.left = newNode
+                    return root;
+                }
+                curr = curr.left
+            } else {
+                return root
+            }
+        }
+    }
+}

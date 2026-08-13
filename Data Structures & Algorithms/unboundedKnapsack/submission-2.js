7cfaa8c0ba1c1a9a2ce9556c36bdaa0ce45ddecc
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-12 21:20:34 -0700

    Add: unboundedKnapsack - submission-2

---
+++ Data Structures & Algorithms/unboundedKnapsack/submission-2.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {<Array<number>} profit
+     * @param {<Array<number>} weight
+     * @param {number} capacity
+     * @returns {number}
+     */
+    maximumProfit(profit, weight, capacity) {
+        const memo = new Map()
+        const dfs = (i, cap) => {
+            if (i >= profit.length) return 0;
+            const key = `${i},${cap}`;
+            if (memo.has(key)) return memo.get(key);
+            const skip = dfs(i + 1, cap);
+            let pick = -Infinity;
+            if (cap >= weight[i]) {
+                pick = dfs(i, cap - weight[i]) + profit[i];
+            }
+            const res = Math.max(pick,skip);
+            memo.set(key,res)
+            return res;
+        }
+
+
+
+        return dfs(0,capacity)
+    }
+}

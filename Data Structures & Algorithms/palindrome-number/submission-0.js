11d5b0608721e7c07c18ec290e4b894dbf85a380
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-18 20:51:26 -0700

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {boolean}
+     */
+    isPalindrome(x) {
+        if (x < 0) return false;
+        let rev = 0;
+        let num = x
+        while (num > 0) {
+            const digit = num % 10
+            rev = rev * 10 + digit
+            num = Math.floor(num / 10)
+        }
+        return rev === x;
+    }
+}

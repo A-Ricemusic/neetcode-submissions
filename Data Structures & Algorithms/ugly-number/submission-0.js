a8fd926ce888e68958dbf862ae51057c8b8387de
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-19 17:32:12 -0700

    Add: ugly-number - submission-0

---
+++ Data Structures & Algorithms/ugly-number/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isUgly(n) {
+        if (n < 0) return false;
+
+        for (const p of [2,3,5]) {
+            while (n % p === 0) {
+                n = n / p;
+            }
+        }
+
+        return n === 1
+
+    }
+}

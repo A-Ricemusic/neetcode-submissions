fbb3acbd48435f23c965ffac750816c18d53d5c7
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-20 10:40:48 -0700

    Add: segmentTree - submission-1

---
+++ Data Structures & Algorithms/segmentTree/submission-1.js
@@ -0,0 +1,75 @@
+class SegmentTreeNode {
+  constructor(sum,L,R) {
+    this.sum = sum
+    this.left = null
+    this.right = null
+    this.L = L
+    this.R = R
+  }
+
+
+}
+
+
+class SegmentTree {
+    /**
+     * @param {number[]} nums
+     */
+    constructor(nums) {
+      this.root = this.build(nums,0, nums.length - 1);
+    }
+
+    build(nums,L,R) {
+      if (L === R) return new SegmentTreeNode(nums[L],L,R)
+
+      const M = Math.floor(L + (R - L) / 2);
+      const Root = new SegmentTreeNode(0,L,R);
+      Root.left = this.build(nums,L,M);
+      Root.right = this.build(nums,M + 1,R);
+      Root.sum = Root.left.sum + Root.right.sum
+      return Root;
+    }
+
+    /**
+     * @param {number} index
+     * @param {number} val
+     */
+    update(index, val) {
+      this.updateHelper(this.root,index,val)
+    }
+
+
+    updateHelper(node,index,val) {
+      if (node.L === node.R) {
+        node.sum = val;
+        return
+      };
+
+      const M = Math.floor(node.L + (node.R - node.L) / 2)
+      if (index <= M) {
+        this.updateHelper(node.left,index,val)
+      } else {
+        this.updateHelper(node.right,index,val)
+      }
+      node.sum = node.left.sum + node.right.sum
+    }
+
+    /**
+     * @param {number} L
+     * @param {number} R
+     * @returns {number}
+     */
+    query(L, R) {
+      return this.queryHelper(this.root,L,R)
+    }
+
+    queryHelper(root,L,R) {
+      if (L <= root.L && root.R <= R) return root.sum
+      if (R < root.L || L > root.R) return 0;
+
+      return (
+        this.queryHelper(root.left,L,R) +
+        this.queryHelper(root.right,L,R)
+      )
+    }
+}

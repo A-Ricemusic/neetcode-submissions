ceb8d29e06c3596fb6c6f547f79c47b776e2367a
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-24 20:51:08 -0700

    Add: quickSort - submission-2

---
+++ Data Structures & Algorithms/quickSort/submission-2.js
@@ -0,0 +1,38 @@
+/** Pair class to store key-value pairs */
+// class Pair {
+//   /**
+//    * @param {number} key The key to be stored in the pair
+//    * @param {string} value The value to be stored in the pair
+//    */
+//   constructor(key, value) {
+//       this.key = key;
+//       this.value = value;
+//   }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[]}
+     */
+    quickSort(pairs) {
+        this.helper(pairs,0, pairs.length - 1);
+        return pairs
+    }
+
+
+    helper(pairs,s,e) {
+        if (e - s + 1 <= 1) return 
+        const pivot = e;
+        let l = s
+        for (let i = s; i < e; i++) {
+            if (pairs[i].key < pairs[pivot].key) {
+                [pairs[l], pairs[i]] = [pairs[i], pairs[l]];
+                l++;
+            }
+        }
+        [pairs[l], pairs[pivot]] = [pairs[pivot], pairs[l]];
+
+        this.helper(pairs,s, l - 1);
+        this.helper(pairs, l + 1, e)
+    }
+}

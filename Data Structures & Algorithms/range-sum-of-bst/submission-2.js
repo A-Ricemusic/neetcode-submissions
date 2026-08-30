3e88e466d2570a192032130f884c2049ba920d73
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-08-30 16:29:58 -0700

    Add: range-sum-of-bst - submission-2

---
+++ Data Structures & Algorithms/range-sum-of-bst/submission-2.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} low
+     * @param {number} high
+     * @return {number}
+     */
+    rangeSumBST(root, low, high) {
+
+        let res = 0
+        const dfs = (curr) => {
+            if (!curr) return;
+            if (curr.val >= low && curr.val <= high) {
+                res += curr.val;
+                dfs(curr.left);
+                dfs(curr.right);
+            } else if (curr.val < low) {
+                dfs(curr.right);
+            } else if (curr.val > high) {
+                dfs(curr.left);
+            } 
+        }
+        dfs(root);
+     
+
+        return res;
+    }
+}

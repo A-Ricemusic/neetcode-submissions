f6d3f57365d50f84c3b0e66ff3f946f321b5db6a
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-03 16:20:30 -0700

    Add: greatest-common-divisor-traversal - submission-4

---
+++ Data Structures & Algorithms/greatest-common-divisor-traversal/submission-4.js
@@ -0,0 +1,76 @@
+class UnionFind {
+    constructor(n) {
+        this.par = new Array(n).fill(0);
+        this.rank = new Array(n).fill(0);
+        this.size = n;
+        for (let i = 0; i < n; i++) {
+            this.par[i] = i
+        }
+    }
+
+    find(x) {
+        let p = this.par[x]
+        while (p !== this.par[p]) {
+            this.par[p] = this.par[this.par[p]]
+            p = this.par[p]
+        }
+        return p;
+    }
+
+    union(x,y) {
+        const p1 = this.find(x);
+        const p2 = this.find(y);
+        if (p1 === p2) return false;
+        if (this.rank[p1] > this.rank[p2]) {
+            this.par[p2] = p1;
+        } else if (this.rank[p2] > this.rank[p1]) {
+            this.par[p1] = p2;
+        } else {
+            this.par[p2] = p1;
+            this.rank[p1]++;
+        }
+        this.size--;
+        return true
+    }
+}
+
+
+
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canTraverseAllPairs(nums) {
+        const n = nums.length
+        const uf = new UnionFind(n);
+        const factorIndex = new Map() // index of value with factor f
+        for (let i = 0; i < n; i++) {
+            let f = 2;
+            let num = nums[i]
+            while (f * f <= num) {
+                if (num % f === 0) {
+                    if (factorIndex.has(f)) {
+                        uf.union(i, factorIndex.get(f))
+                    } else {
+                       factorIndex.set(f, i)
+                    }
+                    while (num % f === 0) {
+                        num = Math.floor(num / f);
+                    }
+                }
+                f++;
+            }
+            if (num > 1) {
+                if (factorIndex.has(num)) {
+                    uf.union(i,factorIndex.get(num))
+                } else {
+                    factorIndex.set(num, i)
+                }
+            }
+        }
+        return uf.size === 1;
+
+    }
+}

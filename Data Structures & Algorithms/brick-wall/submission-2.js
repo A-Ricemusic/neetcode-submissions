0c0da63d0cba6d8a74dfd3f79b3c9c93fdc4b946
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-06 17:25:54 -0700

    Add: brick-wall - submission-2

---
+++ Data Structures & Algorithms/brick-wall/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[][]} wall
+     * @return {number}
+     * {
+     * 1: 3
+     * 2: 1
+     * 3: 3
+     * 4: 5
+     * 5: 2
+     * }
+     */
+    leastBricks(wall) {
+        const hashMap = new Map();
+
+        for (const row of wall) {
+            let gap = 0;
+            for (let i = 0; i < row.length - 1; i++) {
+                gap += row[i];
+                hashMap.set(gap, (hashMap.get(gap) || 0) + 1);
+            }
+        }
+        let maxGap = 0
+        for (const [_,val] of hashMap.entries()) {
+            maxGap = Math.max(val, maxGap)
+        }
+
+        return  wall.length - maxGap
+    }
+}

4f62b4dfb1987ed2019f6f13c343b50c70b81369
Anthony Rice <108903641+A-Ricemusic@users.noreply.github.com>
2026-09-22 09:51:57 -0700

    Add: insert-delete-getrandom-o1 - submission-0

---
+++ Data Structures & Algorithms/insert-delete-getrandom-o1/submission-0.js
@@ -0,0 +1,43 @@
+class RandomizedSet {
+    constructor() {
+        this.set = new Set();
+
+    }
+
+    /**
+     * @param {number} val
+     * @return {boolean}
+     */
+    insert(val) {
+        if (this.set.has(val)) return false;
+        this.set.add(val)
+        return true;
+    }
+
+    /**
+     * @param {number} val
+     * @return {boolean}
+     */
+    remove(val) {
+        if (!this.set.has(val)) return false;
+        this.set.delete(val);
+        return true;
+    }
+
+    /**
+     * @return {number}
+     */
+    getRandom() {
+        const arr = [...this.set];
+        let randIdx = Math.floor(Math.random(0,arr.length - 1));
+        return arr[randIdx]
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
